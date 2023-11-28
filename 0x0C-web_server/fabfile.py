7c37f5c5b9001b3f01b10ecaa93a8a9e27f62e43
Mokoena13 <ernestmokoena.mokoena@gmail.com>
2023-11-28 03:42:17 -0800

    server

---
+++ 0x0C-web_server/fabfile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Fabfile defining functions to pack, deploy, and clean the
+# current directory to a remote server.
+from fabric import task
+
+
+@task
+def pack(c):
+    """Create a tar gzipped archive of the current directory."""
+    c.run("touch schoolwebapp.tar.gz")
+    c.run("tar --exclude='*.tar.gz' -cvzf holbertonwebapp.tar.gz .")
+
+
+@task
+def deploy(c):
+    """Upload the archive to the remote server in the /tmp/ directory."""
+    c.user = "ubuntu"
+    c.put("schoolwebapp.tar.gz", "/tmp")
+    c.run("mkdir /tmp/schoolwebapp")
+    c.run("tar -C /tmp/schoolwebapp -xzvf /tmp/schoolwebapp.tar.gz")
+
+
+@task
+def clean(c):
+    """Deletes schoolwebapp.tar.gz on the local machine."""
+    c.run("rm schoolwebapp.tar.gz")
